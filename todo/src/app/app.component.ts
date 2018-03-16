f88305b3f5fb39d8752c630a6a5ef3b4a1f9357c
hades <quentindevial19@gmail.com>
2018-03-16 18:56:00 +0100

    first commit

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos$: Observable<Todo[]> = this.todoService.todos$.asObservable();
+  public message: string;
+
+  constructor(private todoService: TodoService) {}
+
+  public addTodo() {
+    this.todoService.addTodo({ message: this.message, done: false });
+  }
+
+  public toggleTodo(index: number) {
+    this.todoService.toggleTodo(index);
+  }
+
+  public deleteTodo(index: number) {
+    console.log(index);
+    this.todoService.deleteTodo(index);
+  }
+
+}
